146e007c6139388a5f36ecefa1a1d67bd99fb0bf
Matthew Hoskin <m.hoskin@warwick.ac.uk>
2019-02-25 12:27:25 +0000

    fixing everything

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("goodbye universe")
-
-=======
-print("goodbye world")
-=======
-print("goodbye universe")
->>>>>>> origin/master
 print("See you space cowboy")
 # I made some changes
->>>>>>> origin/master
